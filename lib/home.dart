2689d0b7486567800ce6c42ecbb4b42d0145eb64
usamafzal <usamaafzall@outlook.com>
2024-11-23 17:02:34 +0500

    Add isolates & refresh indicator

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@
 import 'package:apicall/model.dart'; // Custom model class for data representation
 import 'package:apicall/network.dart'; // Network service class for API calls
 import 'package:flutter/material.dart'; // Flutter's material design package for UI elements
+import 'package:http/http.dart' as http;
 
 // Define a stateful widget for the homepage
 class HomePage extends StatefulWidget {
@@ -24,8 +25,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
 
     // Fetch data using the NetworkService's fetchData method
-    getData = NetworkService.fetchData();
-
+    getData = NetworkService.fetchData(http.Client());
     // Once data is fetched, update the filterData list using setState
     getData.then(
       (value) => setState(
@@ -48,7 +48,8 @@ class _HomePageState extends State<HomePage> {
                   (element) => (element.title!.toLowerCase().contains(
                           query.toLowerCase()) || // Match query in title
                       element.body!.toLowerCase().contains(
-                          query.toLowerCase())), // Match query in body
+                            query.toLowerCase(),
+                          )), // Match query in body
                 )
                 .toList(); // Convert filtered results into a list
           },
@@ -93,47 +94,67 @@ class _HomePageState extends State<HomePage> {
             ),
             // Expanded widget to allow the list to take up remaining space
             Expanded(
-              child: FutureBuilder(
-                future: getData, // Use the fetched data future
-                builder: (context, snapshot) {
-                  // Check if the snapshot has data
-                  if (snapshot.hasData) {
-                    return filterData.isEmpty
-                        ? const Center(
-                            // Display if no data matches the query
-                            child: Text(
-                              "No Data Found☹",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          )
-                        : ListView.builder(
-                            // Build a list view for displaying filtered data
-                            itemCount: filterData.length, // Number of items
-                            itemBuilder: (context, index) {
-                              final data = filterData[index];
-                              return ListTile(
-                                title: Text(
-                                  data.title.toString(), // Display the title
-                                  textAlign: TextAlign.justify,
-                                ),
-                                leading: CircleAvatar(
-                                  // Display the ID in a circular avatar
-                                  child: Text(data.id.toString()),
-                                ),
-                              );
-                            },
-                          );
-                  } else if (snapshot.hasError) {
-                    // Show an error message if an error occurs
-                    return Center(
-                      child: Text(snapshot.error.toString()),
-                    );
-                  }
-                  // Show a loading indicator while fetching data
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
+              child: RefreshIndicator(
+                // This is the RefreshIndicator widget that shows a pull-to-refresh UI.
+                // It triggers the onRefresh callback when the user swipes down on the screen.
+                onRefresh: () async {
+                  // Print a message when the refresh is triggered to indicate the start of data fetching.
+                  debugPrint("Refresh triggered! Data Fetching");
+
+                  // Simulate a delay of 2 seconds to mock data fetching.
+                  await Future.delayed(const Duration(seconds: 2));
+
+                  // Call the getData function to fetch the actual data.
+                  await getData;
+
+                  // After data fetching, call setState to rebuild the widget tree with the new data.
+                  setState(() {});
+
+                  // Print a message after the data has been refreshed and UI updated.
+                  debugPrint("Refresh Data!");
                 },
+                child: FutureBuilder(
+                  future: getData, // Use the fetched data future
+                  builder: (context, snapshot) {
+                    // Check if the snapshot has data
+                    if (snapshot.hasData) {
+                      return filterData.isEmpty
+                          ? const Center(
+                              // Display if no data matches the query
+                              child: Text(
+                                "No Data Found☹",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            )
+                          : ListView.builder(
+                              // Build a list view for displaying filtered data
+                              itemCount: filterData.length, // Number of items
+                              itemBuilder: (context, index) {
+                                final data = filterData[index];
+                                return ListTile(
+                                  title: Text(
+                                    data.title.toString(), // Display the title
+                                    textAlign: TextAlign.justify,
+                                  ),
+                                  leading: CircleAvatar(
+                                    // Display the ID in a circular avatar
+                                    child: Text(data.id.toString()),
+                                  ),
+                                );
+                              },
+                            );
+                    } else if (snapshot.hasError) {
+                      // Show an error message if an error occurs
+                      return Center(
+                        child: Text(snapshot.error.toString()),
+                      );
+                    }
+                    // Show a loading indicator while fetching data
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ),
               ),
             ),
           ]),
--- lib/network.dart
+++ lib/network.dart
@@ -1,5 +1,6 @@
 // Import necessary packages for HTTP requests and JSON decoding
 import 'dart:convert'; // For JSON encoding and decoding
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http; // For HTTP requests
 
 import 'model.dart'; // Model Class for Data Representation
@@ -7,23 +8,29 @@ import 'model.dart'; // Model Class for Data Representation
 // Define a class to handle network-related operations
 class NetworkService {
   // Define a static method to fetch data from a remote server
-  static Future<List<DataModel>> fetchData() async {
+  static Future<List<DataModel>> fetchData(http.Client client) async {
     // URL of the API endpoint
     const url = "https://jsonplaceholder.typicode.com/posts";
 
     // Make an HTTP GET request to the specified URL
-    final response = await http.get(Uri.parse(url));
+    final response = await client.get(Uri.parse(url));
 
     // Check if the response status code indicates success (HTTP 200 OK)
     if (response.statusCode == 200) {
-      // Parse the response body (JSON) into a list of dynamic objects
-      List<dynamic> jsonconvert = jsonDecode(response.body);
-
-      // Convert the list of dynamic objects into a list of DataModel objects
-      return jsonconvert.map((e) => DataModel.fromJson(e)).toList();
+      return compute(parseData, response.body);
     } else {
       // Throw an exception if the HTTP request fails
       throw Exception("Error Fetch Data!");
     }
   }
+}
+
+// Seperate Logic to add Isolates
+List<DataModel> parseData(String responseBody) {
+  // Parse the response body (JSON) into a list of dynamic objects
+
+  List<dynamic> parsedData = jsonDecode(responseBody);
+  // Convert the list of dynamic objects into a list of DataModel objects
+
+  return parsedData.map((e) => DataModel.fromJson(e)).toList();
 }
